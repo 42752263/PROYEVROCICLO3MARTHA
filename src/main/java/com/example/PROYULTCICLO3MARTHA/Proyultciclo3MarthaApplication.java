e9df473c87062637ed0a18da324662bd124e752c
MARTHA GLADYS ESCOBAR <tagla263@gmail.com>
2022-09-08 17:29:55 -0500

    un commit

---
+++ src/main/java/com/example/PROYULTCICLO3MARTHA/Proyultciclo3MarthaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PROYULTCICLO3MARTHA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyultciclo3MarthaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyultciclo3MarthaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PROYULTCICLO3MARTHA/Proyultciclo3MarthaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROYULTCICLO3MARTHA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyultciclo3MarthaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
