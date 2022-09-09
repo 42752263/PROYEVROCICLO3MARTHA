9b4bb7f30b5117e6487f427d78da88a0c377e800
MARTHA GLADYS ESCOBAR <tagla263@gmail.com>
2022-09-08 21:43:17 -0500

    Se crearon las estructuras Repositorios y Servcios Empleado, Empresa y MovimientoDinero.

---
+++ src/main/java/com/example/PROYULTCICLO3MARTHA/repo/MovimientosRepositorio.java
@@ -0,0 +1,4 @@
+package com.example.PROYULTCICLO3MARTHA.repo;
+
+public interface MovimientosRepositorio {
+}
---
+++ src/main/java/com/example/PROYULTCICLO3MARTHA/servis/EmpledoService.java
@@ -0,0 +1,4 @@
+package com.example.PROYULTCICLO3MARTHA.servis;
+
+public class EmpledoService {
+}
